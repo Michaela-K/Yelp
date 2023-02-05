e6c9ba64391e86f2b7dc285ccdf004c5a286866d
Michaela-K <michaela.kodes@gmail.com>
2023-02-05 08:36:58 -0500

    add files to yelp

---
+++ db practice.sql
@@ -0,0 +1,8 @@
+CREATE TABLE restaurants(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  price_range INT NOT NULL check(price_range >= 1 and price_range <=5)
+);
+
+Insert into restaurants (name, location, price_range) values ('Panera', 'Markham, Ontario', 4);
