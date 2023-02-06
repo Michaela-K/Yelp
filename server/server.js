0b7ae367613af8f8df321b6be9b8cd18ae3c15a2
Michaela-K <michaela.kodes@gmail.com>
2023-02-06 08:40:02 -0500

    add port variable

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@
 const express = require("express")
 
 //create an instance of express and stored it in a variable called app
+const port = 3001;
 const app = express()
 //make the app listen on a specific port and then do actions after it starts up
-app.listen(3001, () =>{
-  console.log("server is up and listening on port 3001")
+app.listen(port, () =>{
+  console.log(`server is up and listening on port ${port}`)
 });
