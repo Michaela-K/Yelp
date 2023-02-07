29b9bbd126ba3d3259cb431b8617acf7f5e505dc
Michaela-K <michaelakodes@gmail.com>
2023-02-07 08:50:02 -0500

    add .env

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 //This file createa and initialises the app
-//import dtenv before anything else
+//import dotenv before anything else
 require("dotenv").config();
 //import express
 const express = require("express")
 
 //create an instance of express and stored it in a variable called app
-const port = 3001;
+const port = process.env.PORT || 3002;
 const app = express()
 //make the app listen on a specific port and then do actions after it starts up
 app.listen(port, () =>{
