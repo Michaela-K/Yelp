db25892f9cd366734e2979f4172525fa84f4db25
Michaela-K <michaelakodes@gmail.com>
2023-02-08 07:00:12 -0500

    add nodemon

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ const express = require("express")
 //create an instance of express and stored it in a variable called app
 const port = process.env.PORT || 3002;
 const app = express()
+
+//to create a route we have to reference the app
+//the reponse will be stored in "res"
+app.get("restaurant", (req, res) => {
+  console.log("get all restaurants")
+})  
+
 //make the app listen on a specific port and then do actions after it starts up
 app.listen(port, () =>{
   console.log(`server is up and listening on port ${port}`)
