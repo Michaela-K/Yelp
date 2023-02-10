9fb059ccf8bd0d9bb23b40861abd912e6d24d024
Michaela-K <michaelakodes@gmail.com>
2023-02-10 08:50:02 -0500

    morgan

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 //This file createa and initialises the app
 //import dotenv before anything else
 require("dotenv").config();
-//import express
+//import express & morgan
 const express = require("express")
-
+const morgan = require("morgan")
 //create an instance of express and stored it in a variable called app
 const port = process.env.PORT || 3002;
 const app = express()
 
+//MIDDLEWARE CUSTOM EXAMPLE
 //This needs to be first or it would never be used, it would hit the other routes and stop
 //put middleware below app(it does some work before sending to next step which is the route handler->(req, res))
 app.use((req, res, next) => {  //next allows the middleware to pass the request fown to the route handler -> req, res
   console.log("the middleware")
   next();
 });
+
+//third party middleware already call next, so you dont have to actually call it in the function
+app.use(morgan("dev"));
 
 //to create a route we have to reference the app
 // re and res are referred to as route handlers
