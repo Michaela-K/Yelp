7e5cfef656e24c357d847ec5adae93121cce2b94
Michaela-K <michaelakodes@gmail.com>
2023-02-12 11:27:03 -0500

    add paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>React Application</div>
+  return <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/restaurant/:id/update" element={<RestaurantDetailPage/>}/>
+          <Route path="/restaurant/:id" element={<UpdatePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
 }
 
 export default App;
