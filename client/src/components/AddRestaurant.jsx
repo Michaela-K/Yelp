3c549cc3e26a1facf8caaf6397c698b7988cd955
Michaela-K <michaelakodes@gmail.com>
2023-02-12 12:52:05 -0500

    addRestaurant compoenent

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const AddRestaurant = () => {
   return (
-    <div>AddRestaurant</div>
+    <div className='mb-4'>
+      {/* Form */}
+      <form action=''>
+        {/* Row */}
+        <div className="form-row d-flex flex-row justify-content-center">
+          {/* Columns */}
+          <div className="col col-xl-auto">
+            <input type="text" className='form-control' placeholder='Name' />
+          </div>
+          <div className="col col-xl-auto">
+            <input type="text" className='form-control' placeholder='Location' />
+          </div>
+          <div className="col col-lg-auto">
+            <select type="text" className='custom-select' placeholder='Location'>
+              <option disabled> Price Range</option>
+              <option value={1}>$</option>
+              <option value={2}>$$</option>
+              <option value={3}>$$$</option>
+              <option value={4}>$$$$</option>
+              <option value={5}>$$$$$</option>
+            </select>
+          </div>
+            <button className='btn btn-primary btn-md'>Add</button>
+        </div>
+      </form>
+
+    </div>
   )
 }
 
