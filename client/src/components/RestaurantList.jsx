a833bdf4028b25c26766d9024d9ae671950acd2e
Michaela-K <michaelakodes@gmail.com>
2023-02-12 22:22:37 -0500

    retrieve and add data to context api

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import RestaurantFinder from '../apis/RestaurantFinder'
+import { RestaurantsContext } from '../context/RestaurantsContext'
 
-const RestaurantList = () => {
+const RestaurantList = (props) => {  //everything has access to context api beacuse it wraps it in App.js
+  //import use context and the context and store the data retrieved
+  const {restaurants, setRestaurants} = useContext(RestaurantsContext)
   //to get data to mount as soon as you open is by using useEffect, This runs when the component mounts and re renders, unless you put empty []
   useEffect(() => {
     //this use effect hook DOES NOT LIKE IT when we return anything, thats why we need to put it in this fetchData function and call it. This function returns, no the useEffect
@@ -9,6 +12,7 @@ const RestaurantList = () => {
     try{
       const response =  await RestaurantFinder.get("/"); // here its just a slash because it goes to the baseURL in the api file
     console.log(response);  //this use effect hook does not like it when we return anything
+    setRestaurants(response.data.data.restaurants)
     }catch (err){};
   };
     fetchData();
