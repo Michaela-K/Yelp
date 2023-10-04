0e52c4a75d0cef1bf888e23d6c37c57c52cf7d09
Michaela-K <michaelakodes@gmail.com>
2023-10-04 09:21:02 -0400

    make notes on the async await implicitly returning a promise in the try catch and also implement promises to replace the try catch and deal with the returned promise

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-export default axios.create({
+//axios is easier than fetch in some ways
+export default axios.create({ //creates an axios instance
   baseURL: "http://localhost:3001/api/v1/restaurants",
 })
--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -3,19 +3,38 @@ import RestaurantFinder from '../apis/RestaurantFinder'
 import { RestaurantsContext } from '../context/RestaurantsContext'
 
 const RestaurantList = (props) => {  //everything has access to context api beacuse it wraps it in App.js
-  //import use context and the context and store the data retrieved
-  const {restaurants, setRestaurants} = useContext(RestaurantsContext)
-  //to get data to mount as soon as you open is by using useEffect, This runs when the component mounts and re renders, unless you put empty []
+  //import use context and the context to store the data retrieved
+  const {restaurants, setRestaurants} = useContext(RestaurantsContext)  //restaurants and setRestaurants comes from the RestaurantContext.js
+  //to get/fetch the data to mount as soon as you open is by using useEffect, This runs when the component 1) mounts and when it 2)re renders, unless you put empty []
+
   useEffect(() => {
-    //this use effect hook DOES NOT LIKE IT when we return anything, thats why we need to put it in this fetchData function and call it. This function returns, no the useEffect
-    const fetchData = async () => {
-    try{
-      const response =  await RestaurantFinder.get("/"); // here its just a slash because it goes to the baseURL in the RestaurantFinder lapi file
-    console.log(response);  //this use effect hook does not like it when we return anything
-    setRestaurants(response.data.data.restaurants)
-    }catch (err){};
-  };
-    fetchData(); 
+    //USING TRY CATCH 
+
+  //   //this use effect hook DOES NOT LIKE IT when we return anything, thats why we need to put it in this fetchData function and call it. 
+  //   //This function returns, no the useEffect
+  //   const fetchData = async () => {
+  //   try{
+  //     const response =  await RestaurantFinder.get("/"); // here its just a slash because it goes to the baseURL in the RestaurantFinder api file and adds a slah to the emd of that baseURL
+  //     console.log(response);  //this use effect hook does not like it when we return anything
+  //     //When we use async await, we implicitly return a Promise which useEffect does not like, so we created this fetchData fn to deal with it so the useEffect isnt returning anything, the fn is.
+  //     setRestaurants(response.data.data.restaurants)
+  //   }catch (err){
+  //     console.log(err.message);
+  //   };
+  // };
+  //   fetchData(); 
+
+    //USING PROMISES
+  const response = RestaurantFinder.get("/");
+  response
+    .then((response) => {
+      setRestaurants(response.data.data.restaurants)  //store the restaurants 
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+
   }, [])  //empty array is put here so it will only run when it mounts and never again
 
   return (
